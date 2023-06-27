a925c319bd297ad9a1efe5dcf8e3245fb6383d78
Atharv <atharvpatil5177@gmail.com>
2023-06-27 23:10:39 +0530

    api integrated with the ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,29 @@ import { useState } from "react";
 import Axios from "axios";
 
 function App() {
+  const [excuse, setExcuse] = useState("");
+  const fetchData = async (excuseCategory) => {
+    const response = await Axios.get(
+      `https://excuser-three.vercel.app/v1/excuse/${excuseCategory}`
+    );
+    setExcuse(response.data[0].excuse);
+    console.log(response);
+  };
   return (
     <>
       <h1>Generate Excuse</h1>
-      <button>Family Excuse</button>
-      <button>Office Excuse</button>
-      <button>Children Excuse</button>
-      <button>College Excuse</button>
-      <button>Party Excuse</button>
-      <button>Funny Excuse</button>
-      <button>Unbelievable Excuse</button>
-      <button>Developers Excuse</button>
-      <button>Gaming Excuse</button>
+      <button onClick={() => fetchData("family")}>Family Excuse</button>
+      <button onClick={() => fetchData("office")}>Office Excuse</button>
+      <button onClick={() => fetchData("children")}>Children Excuse</button>
+      <button onClick={() => fetchData("college")}>College Excuse</button>
+      <button onClick={() => fetchData("party")}>Party Excuse</button>
+      <button onClick={() => fetchData("funny")}>Funny Excuse</button>
+      <button onClick={() => fetchData("unbelievable")}>
+        Unbelievable Excuse
+      </button>
+      <button onClick={() => fetchData("developers")}>Developers Excuse</button>
+      <button onClick={() => fetchData("gaming")}>Gaming Excuse</button>
+      <h1>{excuse}</h1>
     </>
   );
 }
