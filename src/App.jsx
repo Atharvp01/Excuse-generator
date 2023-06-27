4546ca25e5a59d39bda4d0e02d1260eecff7bf0c
Atharv <atharvpatil5177@gmail.com>
2023-06-27 22:51:38 +0530

    UI Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <h1>Generate Excuse</h1>
+      <button>Family Excuse</button>
+      <button>Office Excuse</button>
+      <button>Children Excuse</button>
+      <button>College Excuse</button>
+      <button>Party Excuse</button>
+      <button>Funny Excuse</button>
+      <button>Unbelievable Excuse</button>
+      <button>Developers Excuse</button>
+      <button>Gaming Excuse</button>
     </>
   );
 }
